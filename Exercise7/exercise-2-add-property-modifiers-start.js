c84e755ba04035cabc0d87c383a6a0159d60e782
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-12 19:34:44 +0100

    Add files via upload

---
+++ Exercise7/exercise-2-add-property-modifiers-start.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const countryA = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+const countryB = {
+    name: "Italy",
+    code: "IT"
+};
+const countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795,
+};
+const currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
+currencyData.name = "Gold";
+currencyData.code = "GGG";
+
+console.log(countryA, countryB, countryC)
