232d7b21ea435c52a4b364830900712c0db4276e
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-12 19:31:15 +0100

    Add files via upload

---
+++ Exercise4/exercise-4-type-aliases-start.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const country1 = "Nigeria";
+const country2 = "Italy";
+const country3 = "China";
+
+console.log(country1, country2, country3)
