1564d627d555f08bd5a845fe769518a45b2a4447
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-12 19:43:41 +0100

    Add files via upload

---
+++ Exercise15/exercise-1-complete-the-class-start.js
@@ -0,0 +1,33 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+class Currency {
+    // Add types to the constructor parameters.
+    constructor(name, code, symbol) {
+        this.name = name;
+        // Store the `code` and `symbol` parameter values in
+        // their corresponding class fields.
+    }
+    // Methods are public by default.
+    // Add a visibility modifier to make it clear this method is public.
+    //
+    // Add a return type for this method
+    // Hint: Look at the inferred return type.
+    describe() {
+        // Replace the `null` values below with the appropriate field values.
+        // Hint: Access field values with: this.fieldName
+        let description = `The ${null} currency `;
+        description += `has the code ${null} `;
+        description += `and uses the symbol ${null}.`;
+        console.log(description);
+    }
+}
+// ----
+const currencyNaira = new Currency("Naira", "NGN", "₦");
+console.log(currencyNaira);
+// Call the `describe()` method on the `currencyNaira` object.
+// ----
+const currencyUsDollar = new Currency("United States dollar", "USD", "$");
+console.log(currencyUsDollar);
