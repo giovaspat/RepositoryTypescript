9b781097928546ae48bdbd352a337d766d773dc6
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-12 19:33:26 +0100

    Add files via upload

---
+++ Exercise6/exercise-1-define-object-types-start.js
@@ -0,0 +1,24 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add a type annotation to this variable that describes the object it contains.
+const countryPopulation = {
+    code: "NZ",
+    population: 5135300,
+};
+// Define a `Country` interface that describes the object in this variable.
+// Add the `Country` type as a type annotation for this variable.
+const countryData = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+// Define a `Currency` type alias that describes the object in this variable.
+// Add the `Currency` type as a type annotation for this variable.
+const currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
