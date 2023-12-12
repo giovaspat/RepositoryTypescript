50a5d48614d7cc7972fbc350b0ee4055531636ac
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-12 19:50:00 +0100

    Add files via upload

---
+++ Exercise21/exercise-1-construct-types-from-types-start.js
@@ -0,0 +1,35 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const countryA = {
+    code: "CN",
+    population: 1412600000,
+};
+const countryB = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+    population: 10678632,
+    currency: "Euro",
+};
+const countryC = {
+    name: "Italy",
+    code: "IT",
+    languages: ["Italian"],
+    population: 60317116,
+};
+//countryC.population = 60_317_117; --> ERROR
+console.log(countryC);
+const countryD = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5135300,
+};
+const countryE = {
+    name: "Thailand",
+    code: "TH",
+    languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+    currency: "Baht",
+};
