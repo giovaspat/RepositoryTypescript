5bfe7412fda58336d47e7c5a34fef23a56c270b9
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-12 19:45:45 +0100

    Add files via upload

---
+++ Exercise17/exercise-1-create-enums-start.js
@@ -0,0 +1,44 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// The objects in the `countries` array use strings as the values of the
+// `currency` properties.
+// Complete this numeric enum so that it can be used instead of strings.
+// Hint: Use the string values in the `currency` properties as the enum members.
+var Currency;
+(function (Currency) {
+    Currency[Currency["Euro"] = 0] = "Euro";
+    Currency[Currency["Dollar"] = 1] = "Dollar";
+})(Currency || (Currency = {}));
+// Replace the string values for the `currency` properties below with
+const countries = [
+    {
+        name: "France",
+        currency: Currency.Euro,
+    },
+    {
+        name: "United States of America",
+        currency: Currency.Dollar,
+    },
+    {
+        name: "Italy",
+        currency: Currency.Euro,
+    },
+    {
+        name: "New Zealand",
+        currency: Currency.Dollar,
+    },
+];
+// Create a string enum named `LanguageStatus`. Use it to replace the
+// string values for the `status` properties in the objects below.
+var LanguageStatus;
+(function (LanguageStatus) {
+    LanguageStatus[LanguageStatus["primary"] = 0] = "primary";
+    LanguageStatus[LanguageStatus["secondary"] = 1] = "secondary";
+})(LanguageStatus || (LanguageStatus = {}));
+const countryLanguages = [
+    { language: "Spanish", status: LanguageStatus.primary },
+    { language: "English", status: LanguageStatus.secondary },
+];
